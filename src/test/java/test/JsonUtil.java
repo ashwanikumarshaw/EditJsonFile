23920510b4f88a85b51ddefb63fb1a442bf1a8ae
Ashwanikumarshaw <Ashwanikumar11shaw@gmail.com>
2021-08-24 20:53:16 +0530

    update

--- src/test/java/test/JsonUtil.java
+++ src/test/java/test/JsonUtil.java
@@ -15,7 +15,7 @@ public class JsonUtil {
 
 		if (newValue.equalsIgnoreCase("blank"))
 			return;
-		
+
 		String[] trav = attribute.split("\\.");
 
 		// update in outer part
@@ -26,18 +26,17 @@ public class JsonUtil {
 			return;
 		}
 
-		boolean lastcovered = false;
 		JsonObject temp = jsonObject;
 		// iterate to the last and update
-		for (int i = 0; i <trav.length ; i++) {
-		
+		for (int i = 0; i < trav.length; i++) {
+
 			// if next is last
-			if (i  == trav.length - 1) {
-				temp.remove(trav[i ]);
-				temp.addProperty(trav[i ], newValue);
+			if (i == trav.length - 1) {
+				temp.remove(trav[i]);
+				temp.addProperty(trav[i], newValue);
 				return;
 			}
-			
+
 			// array element
 			if (trav[i].contains("[")) {
 				String[] arr = trav[i].split("\\[", 2);
@@ -56,7 +55,55 @@ public class JsonUtil {
 			}
 
 		}
-
 	}
 
+	public static void updateJson(Map<String, String> map, JsonArray jsonarray, String scenario) {
+		String attribute = map.get("Attibute");
+		String newValue = map.get(scenario);
+
+		if (newValue.equalsIgnoreCase("blank"))
+			return;
+
+		String[] trav = attribute.split("\\.");
+
+		JsonObject jsonObject = new JsonObject();
+		jsonObject.add("array", jsonarray);
+		// update in outer part
+
+		if (trav.length == 1) {
+			jsonObject.remove(attribute);
+			jsonObject.addProperty(attribute, newValue);
+			return;
+		}
+
+		JsonObject temp = jsonObject;
+		// iterate to the last and update
+		for (int i = 0; i < trav.length; i++) {
+
+			// if next is last
+			if (i == trav.length - 1) {
+				temp.remove(trav[i]);
+				temp.addProperty(trav[i], newValue);
+				return;
+			}
+
+			// array element
+			if (trav[i].contains("[")) {
+				String[] arr = trav[i].split("\\[", 2);
+				String arrName = arr[0];
+				int row = Integer.parseInt(arr[1].split("\\]", 2)[0]);
+
+				JsonArray points = (JsonArray) temp.get(arrName).getAsJsonArray();
+
+				temp = (JsonObject) points.get(row);
+
+			} // non array element
+			else {
+
+				temp = (JsonObject) temp.get(trav[i]);
+
+			}
+		}
+
+	}
 }
--- src/test/java/test/jsonHandle.java
+++ src/test/java/test/jsonHandle.java
@@ -8,14 +8,18 @@ import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 public class jsonHandle {
 
 	public static void main(String[] args) {
+	
 
 		String path = "D:\\INSTALL\\QAE\\eclipse_Workspace\\editJson\\src\\test\\java\\inputfile\\input0.json";
+		//path="D:\\INSTALL\\QAE\\eclipse_Workspace\\editJson\\src\\test\\java\\inputfile\\input1.json";
+		
 		File inputFile = new File(path);
 
 		String reader = null;
@@ -26,26 +30,52 @@ public class jsonHandle {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		char first = reader.charAt(0);
 
-		JsonObject jsonObject = JsonParser.parseString(reader).getAsJsonObject();
+		JsonObject jsonObject;
+		JsonArray jsonarray;
+		if (first == '{') {
+			jsonObject = JsonParser.parseString(reader).getAsJsonObject();
+			System.out.println("jsonObject old" + jsonObject);
 
-		System.out.println("jsonObject old" + jsonObject);
+			String scenario = "a001";
+			Map<String, String> m = new HashMap<>();
 
-		String scenario = "a001";
-		Map<String, String> m = new HashMap<>();
+			m.put("Attibute", "Phones[0].sec.code");
+			// test : firstName
+			// Phones[0].sec.code
+			// plugin.acc.data.point
+			// Phones[0].Mobile
+			// a[0].c[1].e
+			// a1.b1[0].d[0].e
 
-		m.put("Attibute", "Phones[0].sec.code");
-		// test : firstName
-		// Phones[0].sec.code
-		// plugin.acc.data.point
-		// Phones[0].Mobile
-		// a[0].c[1].e
-		// a1.b1[0].d[0].e
+			m.put(scenario, "Ashwani");
+			JsonUtil.updateJson(m, jsonObject, scenario);
+
+			System.out.println("jsonObject new" + jsonObject);
+			}
+		else if (first == '[') {
+			jsonarray = JsonParser.parseString(reader).getAsJsonArray();
+			System.out.println("jsonObject old" + jsonarray);
+
+			String scenario = "a001";
+			Map<String, String> m = new HashMap<>();
+
+			m.put("Attibute", "array[0].a");
+			// test : firstName
+			// Phones[0].sec.code
+			// plugin.acc.data.point
+			// Phones[0].Mobile
+			// a[0].c[1].e
+			// a1.b1[0].d[0].e
+
+			m.put(scenario, "Ashwani");
+			JsonUtil.updateJson(m, jsonarray, scenario);
+
+			System.out.println("jsonObject new" + jsonarray);
+			}
+
 		
-		m.put(scenario, "Ashwani");
-		JsonUtil.updateJson(m, jsonObject, scenario);
-
-		System.out.println("jsonObject new" + jsonObject);
 	}
 
 }
